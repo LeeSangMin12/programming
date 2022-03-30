802572a779255b29fd98c297562ad8f5ee56cd5e
LeeSangMin12 <52191287+LeeSangMin12@users.noreply.github.com>
2022-03-30 17:15:36 +0900

    코딩알려주는누나
    리액트 강의 1~2

---
+++ 코딩알려주는누나/react-first-day/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Box from './Box'
+
+function App() {
+  return (
+		<main>
+			<Box name="코알누" num="1"/>
+			<Box name="지민" num="2"/>
+			<Box name="혜진" num="3"/>
+		</main>
+  );
+}
+
+export default App;
