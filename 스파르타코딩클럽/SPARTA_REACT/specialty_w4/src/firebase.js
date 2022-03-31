f268e468677700bbe37ea6119f1897f94f924f53
LeeSangMin12 <52191287+LeeSangMin12@users.noreply.github.com>
2022-03-31 22:02:02 +0900

    week4 homework finish

--- 스파르타코딩클럽/SPARTA_REACT/specialty_w4/src/firebase.js
+++ 스파르타코딩클럽/SPARTA_REACT/specialty_w4/src/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDoI17v7aH5XVUfxAgBF8l124ELiJ1_ok8",
-  authDomain: "sparta-react-basic-6ff45.firebaseapp.com",
-  projectId: "sparta-react-basic-6ff45",
-  storageBucket: "sparta-react-basic-6ff45.appspot.com",
-  messagingSenderId: "817666115993",
-  appId: "1:817666115993:web:e2ee244e8ba423557d6b08",
-  measurementId: "G-4J0NEHGCBJ"
+  apiKey: "AIzaSyBJggpW6EI9HzufGwKNE_QnMzXMxEIr-3I",
+  authDomain: "sparta-react-basic-3574c.firebaseapp.com",
+  projectId: "sparta-react-basic-3574c",
+  storageBucket: "sparta-react-basic-3574c.appspot.com",
+  messagingSenderId: "170606414137",
+  appId: "1:170606414137:web:5d129bcc2acc3cf1a040e8",
+  measurementId: "G-FKHKMTPP24"
 };
 
 // Initialize Firebase
