90f0683693b4e4609e8ad2d4c5b93126d7a04daa
LeeSangMin12 <52191287+LeeSangMin12@users.noreply.github.com>
2022-03-31 13:32:15 +0900

    ,
    ,

---
+++ 스파르타코딩클럽/SPARTA_REACT/script.js
@@ -0,0 +1,34 @@
+function sayHello(event){
+  console.log("hello");
+}
+
+const wraps = document.getElementsByClassName("wrap");
+
+const title = document.getElementById('title');
+title.style.backgroundColor = "yellow";
+
+const buttons = document.getElementsByTagName("button");
+for(let i=0; i<buttons.length; i++){
+  buttons[i].addEventListener("click", sayHello);
+}
+
+function changeBackgroundColor(index){
+  let card = document.getElementsByClassName("todo-card")[index];
+  card.style.backgroundColor = "gray";
+}
+
+const new_div = document.createElement("div");
+new_div.style.backgroundColor="green";
+new_div.style.height="100px";
+new_div.style.width="100px";
+document.body.appendChild(new_div)
+
+const dogs = ["검은 강아지", "노란 강아지", "흰 강아지"];
+const cats = ["검은 강아지", "복슬 고양이", "노란 고양이"];
+
+const answer = dogs.concat(cats);
+
+const set = new Set(answer);
+const uniqueArr = [...set];
+
+console.log(uniqueArr);
